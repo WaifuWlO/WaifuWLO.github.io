12845f659506bfa3ec01696cdd2eae9a40553363
WaifuWlO <37492845+WaifuWlO@users.noreply.github.com>
2018-03-18 11:15:58 -0700

    Update testscript.js

--- aframetest/testscript.js
+++ aframetest/testscript.js
@@ -9,3 +9,21 @@ AFRAME.registerComponent('test01', {
   pause: function () {},
   play: function () {}
 });
+
+var boxEl = document.querySelector('a-box');
+boxEl.setAttribute('width', 5);
+boxEl.setAttribute('color', '#FFF');
+boxEl.addEventListener('click', function () {
+  // If we are using the cursor component.
+  boxEl.setAttribute('color', '#FFF');
+});
+boxEl.emit('some-event');
+boxEl.removeAttribute('color');
+boxEl.querySelectorAll('a-sphere');
+
+var sphereEl = document.createElement('a-sphere');
+sphereEl.setAttribute('radius', 1);
+document.querySelector('a-scene').appendChild(sphereEl);
+sphereEl.addEventListener('loaded', function () {
+  console.log('sphere attached');
+});
